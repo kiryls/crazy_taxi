bb2b66fb4458f10ba4a7d6dce6246cdf1406a810
kiryls <kiryl.shvachka@edu.unito.it>
2021-01-25 03:00:32 +0100

    major changes

--- src/common.c
+++ src/common.c
@@ -7,7 +7,7 @@ int load(){
 
     fp = fopen(CONFIG, "r");
     if(fp == NULL) return 1; 
-
+    
     config = (Config *) malloc(sizeof(Config));
     if(config == NULL) return 1;
 
@@ -32,7 +32,7 @@ int load(){
     /* crazione semaforo per sincronizzare la partenza della sim */
     sync_semaphore_id = semget(IPC_PRIVATE, 1, IPC_CREAT | 0600);
     TEST_ERROR;
-    semctl(sync_semaphore_id, 0, SETVAL, config->SO_SOURCES + config->SO_TAXI); 
+    semctl(sync_semaphore_id, 0, SETVAL, config->SO_SOURCES /* + config->SO_TAXI */); 
 
     return 0;
 }
@@ -40,11 +40,14 @@ int load(){
 void init_world () {
     int i, j;
 
-    map_id = shmget(IPC_PRIVATE, sizeof(Cell) * SO_WIDTH*SO_HEIGHT, S_IRUSR | S_IWUSR);
-    TEST_ERROR;
+    map_id = shmget(IPC_PRIVATE, sizeof(int) * SO_HEIGHT, /* S_IRUSR | S_IWUSR */ IPC_CREAT | 0600);
+    map_row_ids = shmat(map_id, NULL, 0);
 
-    map = (Cell**) shmat(map_id, NULL, 0);
-    TEST_ERROR;
+    for(i = 0; i < SO_HEIGHT; i++) {
+        map_row_ids[i] = shmget(IPC_PRIVATE, sizeof(Cell) * SO_WIDTH, IPC_CREAT | 0600);
+        map[i] = shmat(map_row_ids[i], NULL, 0);
+    }
+    
 
     for(i = 0; i < SO_HEIGHT; i++) {
         for(j = 0; j < SO_WIDTH; j++) {
@@ -64,7 +67,7 @@ void init_world () {
         for(j = 0; j < SO_WIDTH; j++) {
             if(!map[i][j].is_hole) {
                 map[i][j].cap_semid = semget(IPC_PRIVATE, 1, IPC_CREAT | 0600); /* semaforo della capienza */
-                TEST_ERROR;
+                /* TEST_ERROR; */
                 semctl(map[i][j].cap_semid, 0, SETVAL, map[i][j].cell_cap); 
             }
         }
@@ -72,17 +75,17 @@ void init_world () {
 
     gen_sources();
 
-    for(i = 0; i < SO_HEIGHT; i++) {
+    /* for(i = 0; i < SO_HEIGHT; i++) {
         for(j = 0; j < SO_WIDTH; j++) {
             if(map[i][j].source_pid > 0) {
-                map[i][j].req_access_sem = semget(IPC_PRIVATE, 1, IPC_CREAT | 0600); /* semaforo per l'accesso alla queue */
-                TEST_ERROR;
+                map[i][j].req_access_sem = semget(IPC_PRIVATE, 1, IPC_CREAT | 0600); 
+                
                 semctl(map[i][j].req_access_sem, 0, SETVAL, 1); 
 
-                pipe(map[i][j].req_pipe); /* pipe delle richieste */
+                pipe(map[i][j].req_pipe); 
             }
         }
-    }
+    } */
 
 
 }
@@ -93,6 +96,7 @@ void gen_holes() {
 
     count = 0; 
     while (count < config->SO_HOLES) {
+        
         i = rand() % SO_HEIGHT;
         j = rand() % SO_WIDTH;
 
@@ -103,12 +107,12 @@ void gen_holes() {
     }
 }
 
-int check_hole(int x, int y){
+int check_hole(int r, int c){
     int i, j;
 
     for(i = -1; i < 3; i++) {
         for(j = -1; j < 3; j++) {
-            if ((y + i >= 0 && y + i < SO_HEIGHT) && (x + j >= 0 && x + j < SO_WIDTH) && map[y + i][x + j].is_hole == 1){
+            if ((r + i >= 0 && r + i < SO_HEIGHT) && (c + j >= 0 && c + j < SO_WIDTH) && map[r + i][c + j].is_hole){
                 return 0;
             }   
         }
@@ -121,10 +125,9 @@ int check_hole(int x, int y){
 
 void gen_sources () {
     int count, child_pid;
-    char ** args;
     int i, j;
+    int exec_ret;
     
-
     count = 0;
     while(count < config->SO_SOURCES) {
 
@@ -140,25 +143,16 @@ void gen_sources () {
                     break;
 
                 case 0:
-                    /* args = malloc((4) * sizeof(char *)); */
-
-                    /* args[0] = malloc(sizeof(int)); */
                     sprintf(args[0],"%d", map_id);
-
-                    /* args[1] = malloc(sizeof(int)); */
                     sprintf(args[1],"%d", i);
-
-                    /* args[2] = malloc(sizeof(int)); */
                     sprintf(args[2],"%d", j);
-
-                    /* args[3] = NULL; */
-
-                    execvp("./source", args);
-
+                    sprintf(args[3],"%d", sync_semaphore_id);
+                    exec_ret = execvp("./out/source", args);
+                    fprintf(stderr, "errore dopo la execvp: %d\n", exec_ret);
                     break;
 
                 default:
-                    map[i][j].source_pid = child_pid;
+                    /* map[i][j].source_pid = child_pid; */
                     if(count == 0) source_gpid = child_pid; /* set first source as group leader */
                     setpgid(child_pid, source_gpid); /* set all sources in the same group */
                     break;
@@ -200,37 +194,44 @@ void sync_simulation(int semid, int nsem, int value){
     sop.sem_num = nsem;
     sop.sem_flg = 0;
     sop.sem_op = value;
-
-    while(semop(semid,&sop,1) == -1) {
+   
+   while(semop(semid,&sop,1) == -1) {
         if(errno != EINTR){
             TEST_ERROR;
         }
     } 
-}
+} 
 
 void unload () {
     int i, j;
+    int ret;
 
-    /* 1. dealloco args */
+    /* 1. args */
     for(i = 0; i < NARGS; i++) free(args[i]);
     free(args);
 
-    /* 2. rimuovo i semafori */
+    /* 2. semafori & pipes */
     for(i = 0; i < SO_HEIGHT; i++) {
         for(j = 0; j < SO_WIDTH; j++) {
             if(!map[i][j].is_hole) semctl(map[i][j].cap_semid, 0, IPC_RMID);
-            if(map[i][j].source_pid > 0) semctl(map[i][j].req_access_sem, 0, IPC_RMID);
+            if(map[i][j].source_pid > 0) {
+              semctl(map[i][j].req_access_sem, 0, IPC_RMID); 
+              close(map[i][j].req_pipe[R]); 
+              close(map[i][j].req_pipe[W]); /* da spostare in gen_sources */
+            }
         }
     }
 
     semctl(sync_semaphore_id, 0, IPC_RMID);
 
-    /* 3. detach & deallocazione map */
-    shmdt(map);
-    TEST_ERROR;
-
-    shmctl(map_id, IPC_RMID, NULL);
-    TEST_ERROR;
+    /* 3. map */
+    for(i = 0; i < SO_HEIGHT; i++) {
+        if(shmdt(map[i])) TEST_ERROR;
+        if(shmctl(map_row_ids[i], IPC_RMID, NULL)) TEST_ERROR;
+    }
+    
+    if (shmdt(map_row_ids)) TEST_ERROR;
+    if(shmctl(map_id, IPC_RMID, NULL)) TEST_ERROR;
 }
 
 void print_map () {
@@ -252,7 +253,7 @@ void print_map () {
                 default:
                     if (map[i][j].source_pid > 0) printf(SOURCE);
                     if(!semctl(map[i][j].cap_semid, 0, GETVAL)) printf(BUSY);
-                    traffic = map[i][j].cell_cap - semctl(map[i][j].cap_semid, 0, GETVAL);
+                    traffic = map[i][j].cell_cap /* - semctl(map[i][j].cap_semid, 0, GETVAL) */;
                     sprintf(s,"%2d ", traffic);
                     printf("%s" ENDSTYLE, traffic == 0 ? "   " : s);
             }
