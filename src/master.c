37b479aa754389bd05d18259bcedc2dd8c5686d8
kiryls <kiryl.shvachka@edu.unito.it>
2021-01-25 23:57:51 +0100

    pause/resume sources. map print in handler. new masking system. wrap up

--- src/master.c
+++ src/master.c
@@ -4,11 +4,11 @@
 int main(int argc, char *argv[]) {
 
     int i, j;
-    char ** args;
     int status;
     int child;
     
     srand(time(NULL));
+
 
     if (load()) exit(EXIT_FAILURE);
     printf("loading completed\n");
@@ -21,21 +21,19 @@ int main(int argc, char *argv[]) {
 
     sync_simulation(sync_semaphore_id, 0, 0);
 
-    printf("si puo' simulare\n");
+    printf("start simulation\n");
+    
+    killpg(source_gpid, SIGCONT);
 
     for(i = 0; i < config->SO_DURATION; i++) {
         sleep(1);
         raise(SIGALRM);
-        print_map();
     }
     raise(SIGQUIT); /* wrap-up the simulation after SO_DURATION seconds */
 
-    
+    /* read(map[i][j].req_pipe[R], &dest, sizeof(Pos)); */
 
-    /* sleep(2); */
-    while((child = wait(&status)) > 0) {
-        printf("(%d) terminated\n", child);
-    }
+    while((child = wait(&status)) > 0) {}
 
     if(errno == ECHILD) printf("all children terminated successfully\n");
     
@@ -45,6 +43,7 @@ int main(int argc, char *argv[]) {
     }
     
     unload();
+
 
     printf("unloaded all structures\n");
 
@@ -78,48 +77,43 @@ void set_signals() {
     struct sigaction sa;
 
     /* set signal mask */
-    sigemptyset(&signal_mask);
-    sigaddset(&signal_mask, SIGALRM); /* every second alarm */
-    sigaddset(&signal_mask, SIGINT);  /* user interruption (CTRL-C) */
-    sigaddset(&signal_mask, SIGQUIT); /* simulation wrap up */
+    sigemptyset(&mask);
+    sigaddset(&mask, SIGALRM); /* every second alarm */
 
     sa.sa_flags = SA_RESTART;
-    sa.sa_mask = signal_mask;
+    sa.sa_mask = mask;
 
     /* set behaviour to print map */
     sa.sa_handler = print_map_handler;
 
     /* put process in listening to SIGALRM */
     sigaction(SIGALRM,&sa,NULL);
-    TEST_ERROR;
+    /* TEST_ERROR; */
 
     /* set behaviour to wrap up */
     sa.sa_handler = wrap_up;
 
     /* put process in listening to SIGQUIT & SIGINT*/
     sigaction(SIGQUIT,&sa,NULL);
-    TEST_ERROR;
+    /* TEST_ERROR; */
     sigaction(SIGINT, &sa, NULL);
-    TEST_ERROR;
+    /* TEST_ERROR; */
 }
 
 void print_map_handler(int sig) {
-    printf("raised periodic alarm\n");
-
-    /* sigprocmask(SIG_BLOCK, &signal_mask, NULL);
-    killpg(taxi_gpid, SIGSTOP);
+    killpg(source_gpid, SIGSTOP);
 
     print_map();
 
-    killpg(taxi_gpid, SIGCONT);
-    sigprocmask(SIG_UNBLOCK, &signal_mask, NULL); */
+    killpg(source_gpid, SIGCONT);
 }
 
 void wrap_up(int sig) {
-    printf("raised wrap up signal\n");
-    /* sigprocmask(SIG_BLOCK, &signal_mask, NULL);
+    /* sigprocmask(SIG_BLOCK, &signal_mask, NULL); */
 
-    sigprocmask(SIG_UNBLOCK, &signal_mask, NULL); */
+    killpg(source_gpid, SIGQUIT);
+
+    /* sigprocmask(SIG_UNBLOCK, &signal_mask, NULL); */
 }
 
 
