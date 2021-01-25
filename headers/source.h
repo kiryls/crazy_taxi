7cfb79f5bba5f86b5cb97a00889e3fed7d95b1b6
kiryls <kiryl.shvachka@edu.unito.it>
2021-01-25 23:53:54 +0100

    added more global vars and methods

--- headers/source.h
+++ headers/source.h
@@ -1,7 +1,11 @@
 
 Pos p;
+int tot_reqs;
+FILE * fp;
 
 void init(const char * argv[]);
+void write_log(FILE * fp, Pos dest);
 void set_signals();
-void signal_handler(int sig);
-void signal_handler (int sig);
+void termination(int sig);
+void gen_req(int sig);
+void resume(int sig);
