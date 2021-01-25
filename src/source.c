0d993e3e82dd82e0b87bb2ead99528aeab278499
kiryls <kiryl.shvachka@edu.unito.it>
2021-01-25 23:59:49 +0100

    pause/resume. gen req. init. termination and resume procedures

--- src/source.c
+++ src/source.c
@@ -5,86 +5,131 @@ int main(int argc, char const *argv[]) {
 
     int i;
     int sem_ret;
+    int count;
 
     srand(getpid());
     
     init(argv);
 
-    printf("child created w/ pid: %d, parent pid: %d, pos (%d,%d)\n", getpid(), getppid(), p.y+1, p.x+1);
-
     set_signals();
     
-    /* chiudo la pipe richieste in lettura */
-    
     sync_simulation(sync_semaphore_id, 0, -1);
-    printf("source %d pronta\n", getpid());
 
-    sleep(6);
+    printf("source %d paused\n", getpid());
+    pause();
+    printf("source %d resumed\n", getpid());
 
-    close(map[p.y][p.x].req_pipe[W]);
-    
-    for(i = 0; i < SO_HEIGHT; i++) shmdt(map[i]);
-    shmdt(map);
-    /* TEST_ERROR; */
 
-    exit(EXIT_SUCCESS);
+    while(1) {
+        sleep(2);
+        raise(SIGALRM);
+    }
+
+    /* for(i = 0; i < 3; i++) raise(SIGALRM); */
 }
 
 void init (const char * argv[]) {
     int i;
 
     map_id = atoi(argv[0]); 
-    p.y = atoi(argv[1]);
-    p.x = atoi(argv[2]);
+    p.r = atoi(argv[1]);
+    p.c = atoi(argv[2]);
     sync_semaphore_id = atoi(argv[3]);
+
+    tot_reqs = 0;
 
     map_row_ids = (int*) shmat(map_id, NULL, 0);
     for(i = 0; i < SO_HEIGHT; i++) map[i] = shmat(map_row_ids[i], NULL, 0);
 
-    map[p.y][p.x].source_pid = getpid();
+    map[p.r][p.c].source_pid = getpid();
 
-    map[p.y][p.x].req_access_sem = semget(IPC_PRIVATE, 1, IPC_CREAT | 0600);
-    semctl(map[p.y][p.x].req_access_sem, 0, SETVAL, 1);
+    map[p.r][p.c].req_access_sem = semget(IPC_PRIVATE, 1, IPC_CREAT | 0600);
+    semctl(map[p.r][p.c].req_access_sem, 0, SETVAL, 1);
 
-    pipe(map[p.y][p.x].req_pipe);
-    close(map[p.y][p.x].req_pipe[R]);
+    close(map[p.r][p.c].req_pipe[R]);
+
+    fp = fopen("/home/kiryls/Documents/Coding/project/logs/src.log", "a");
+}
+
+void write_log(FILE * fp, Pos dest) {
+    fprintf(fp, "%d @ (%d,%d): (PPID=%d | PGID=%d) request #%d -> (%d,%d)\n",
+            getpid(), p.r+1, p.c+1,
+            getppid(), getpgid(getpid()), 
+            tot_reqs, dest.r+1, dest.c+1);
 }
 
 void set_signals () {
     struct sigaction sa;
 
-    sigemptyset(&signal_mask);
-    sigaddset(&signal_mask, SIGALRM);
-    sigaddset(&signal_mask, SIGUSR1);
-    sigaddset(&signal_mask, SIGINT);
-
-    sigfillset(&all_signals);
+    sigemptyset(&mask);
+    sigaddset(&mask, SIGALRM);
+    sigaddset(&mask, SIGQUIT);
+    sigaddset(&mask, SIGINT);
 
     sa.sa_flags = SA_RESTART;
-    sa.sa_mask = signal_mask;
-    sa.sa_handler = signal_handler;
+    sa.sa_mask = mask;
 
+    sa.sa_handler = termination;
+    sigaction(SIGQUIT, &sa, NULL);
+    sigaction(SIGINT, &sa, NULL);
+    
+    sa.sa_handler = gen_req;
+    sigaction(SIGALRM, &sa, NULL);
+
+    sa.sa_handler = resume;
+    sigaction(SIGCONT, &sa, NULL);
 }
 
-void signal_handler (int sig) {
-    sigprocmask(SIG_BLOCK, &signal_mask, NULL);
+void gen_req (int sig) {
+    int i;
+    sigset_t alarm;
+    Pos dest;
 
-    switch (sig) {
-    case SIGALRM: /* source: genera req */
-        break;
+    sigemptyset(&alarm);
+    sigaddset(&alarm, SIGALRM);
 
-    case SIGQUIT: /* master: so_duration */
-        break;
+    /* sigprocmask(SIG_BLOCK, &alarm, NULL); */
 
-    case SIGINT: /* utente: ctrl + c */
-        raise(SIGQUIT);
-        break;
+    do{
+        dest.r = rand() % SO_HEIGHT;
+        dest.c = rand() % SO_WIDTH;
+    }while(map[dest.r][dest.c].is_hole || ( dest.r == p.r && dest.c == p.c));
+
+    P(map[p.r][p.c].req_access_sem, 0);
+        write(map[p.r][p.c].req_pipe[W], &dest, sizeof(Pos));
+    V(map[p.r][p.c].req_access_sem, 0);
+
+    tot_reqs++;
+
+    write_log(fp, dest);
+
+    /* sigprocmask(SIG_UNBLOCK, &alarm, NULL); */
+}
+
+void termination (int sig) {
+    int i;
+    /* sigprocmask(SIG_BLOCK, &mask, NULL); */
+
+    if(sig == SIGINT || sig == SIGQUIT) {
+
+        close(map[p.r][p.c].req_pipe[W]);
     
-    default:
-        printf("source.c (#%3d): unhandled singnal\n", getpid() - getppid());
-        break;
-    }
+        for(i = 0; i < SO_HEIGHT; i++) 
+            if(shmdt(map[i])) TEST_ERROR;
+        if(shmdt(map_row_ids)) TEST_ERROR;
 
-    sigprocmask(SIG_UNBLOCK, &signal_mask, NULL);
+        fclose(fp);
+
+        printf("source (%d) terminated\n", getpid());
+
+        exit(EXIT_SUCCESS);
+
+    }  else printf("source.c (#%3d): unhandled singnal\n", getpid() - getppid());
+
+    /* sigprocmask(SIG_UNBLOCK, &mask, NULL); */
+}
+
+void resume (int sig) {
+    fprintf(fp, "*** resuming source %d ***\n", getpid());
 }
  
