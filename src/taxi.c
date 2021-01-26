30b727e4589cae562d88a994b3779b059e37326b
kiryls <kiryl.shvachka@edu.unito.it>
2021-01-26 01:53:58 +0100

    main, init, resume

--- src/taxi.c
+++ src/taxi.c
@@ -3,41 +3,51 @@
 
 int main(int argc, char const *argv[]) {
     
-    int map_id;
     int i, j;
-    Cell * map;
+
+    init(argv);
+
+    set_signals();
+
+    sync_simulation(sync_taxi_sem, 0, -1);
+
+    pause();
+
+}
+
+void init (const char * argv[]) {
     int i;
 
     map_id = atoi(argv[0]);
+    p.r = atoi(argv[1]);
+    p.c = atoi(argv[2]);
 
-    map = (Cell*) shmat(map_id, NULL, 0);
-    TEST_ERROR;
+    rep.tot_length = 0;
+    rep.tot_time = 0;
+    rep.completed_rides = 0;
 
+    map_row_ids = (int*) shmat(map_id, NULL, 0);
+    for(i = 0; i < SO_HEIGHT; i++) map[i] = shmat(map_row_ids[i], NULL, 0);
 
-    shmdt(map);
-    TEST_ERROR;
-
-    return 0;
+    logp = fopen("/home/kiryls/Documents/Coding/project/logs/taxi.log", "a");
 }
 
 void set_signals () {
     struct sigaction sa;
 
-    sigemptyset(&signal_mask);
-    sigaddset(&signal_mask, SIGALRM);
-    sigaddset(&signal_mask, SIGQUIT);
-    sigaddset(&signal_mask, SIGINT);
-
-    sigfillset(&all_signals);
+    sigemptyset(&mask);
+    sigaddset(&mask, SIGALRM);
+    sigaddset(&mask, SIGQUIT);
+    sigaddset(&mask, SIGINT);
 
     sa.sa_flags = SA_RESTART;
-    sa.sa_mask = signal_mask;
+    sa.sa_mask = mask;
     sa.sa_handler = signal_handler;
 
 }
 
 void signal_handler (int sig) {
-    sigprocmask(SIG_BLOCK, &signal_mask, NULL);
+    sigprocmask(SIG_BLOCK, &mask, NULL);
 
     switch (sig) {
     case SIGALRM:
@@ -53,6 +63,11 @@ void signal_handler (int sig) {
         break;
     }
 
-    sigprocmask(SIG_UNBLOCK, &signal_mask, NULL);
+    sigprocmask(SIG_UNBLOCK, &mask, NULL);
 }
+
+
  
+void resume (int sig) {
+    fprintf(logp, "[resuming taxi %d]\n", getpid());
+}
