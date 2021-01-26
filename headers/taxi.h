8028276fa5b6b36ad1341af04602a14fec2ebd9b
kiryls <kiryl.shvachka@edu.unito.it>
2021-01-26 01:49:03 +0100

    aggiunta struct Report e altre variabili

--- headers/taxi.h
+++ headers/taxi.h
@@ -1,7 +1,20 @@
 #ifndef TAXI
 #define TAXI
 
+typedef struct {
+    /* taxi report on every ride */
+    int tot_length;
+    int tot_time;
+    int completed_rides; 
+} Report;
+
+Pos p;
+int tot_reqs;
+FILE * logp;
+Report rep;
+
 void set_signals();
 void signal_handler(int sig);
+void resume (int sig);
 
 #endif 
