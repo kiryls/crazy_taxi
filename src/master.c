f8d1738f58d6941fc5844ac48c132b73a2b8f904
kiryls <kiryl.shvachka@edu.unito.it>
2021-01-30 19:33:18 +0100

    spostati qui i metodi di cui il master si serve. oltre alla completa riorganizzazione

--- src/master.c
+++ src/master.c
@@ -1,6 +1,12 @@
 #include "../headers/common.h"
 #include "../headers/master.h"
 
+/* 
+################################################################################################# 
+                                            MAIN
+descrizione
+################################################################################################# 
+*/
 int main(int argc, char *argv[]) {
 
     int i, j;
@@ -11,117 +17,465 @@ int main(int argc, char *argv[]) {
 
 
     if (load()) exit(EXIT_FAILURE);
-    printf("loading completed\n");
+        printf("+++ loading completed +++\n");
 
     init_world();
-    printf("world created\n");
+        printf("+++ world created +++\n");
+
+    gen_sources();
+        printf("+++ all sources generated +++\n");
+
+    gen_taxi();
+        printf("+++ all taxi generated +++\n");
+
+    gen_timer();
+        printf("+++ timer %d set +++\n", timer_id);
 
     set_signals();
-    printf("signals set\n");
+        printf("+++ all signals set +++\n");
 
-    sync_simulation(sync_sources_sem, 0, 0);
-    sync_simulation(sync_taxi_sem, 0, 0);
+        printf("\t\t\t~~~ S T A R T ~~~ \n");
+    simulate();
+        printf("\t\t\t ~~~  E  N  D  ~~~ \n");
 
-    printf("start simulation\n");
-    
-    killpg(source_gpid, SIGCONT);
-
-    for(i = 0; i < config->SO_DURATION; i++) {
-        sleep(1);
-        raise(SIGALRM);
-    }
-    raise(SIGQUIT); /* wrap-up the simulation after SO_DURATION seconds */
-
-    while((child = wait(&status)) > 0) {}
-
-    if(errno == ECHILD) printf("all children terminated successfully\n");
-    
-    else {
-        fprintf(stderr, "Error #%d: %s\n", errno, strerror(errno));
-        exit(EXIT_FAILURE);
-    }
     
     unload();
-
-
-    printf("unloaded all structures\n");
+        printf("*** unloaded all structures ***\n");
 
     exit(EXIT_SUCCESS);
+}
+
+/* 
+################################################################################################# 
+                                          LOAD & UNLOAD
+descrizione
+################################################################################################# 
+*/
+int load(){
+    FILE * fp;
+    char key[20];
+    int i;
+
+    fp = fopen(CONFIG, "r");
+    if(fp == NULL) return 1; 
+    
+    config = (Config *) malloc(sizeof(Config));
+    if(config == NULL) return 1;
+
+    fscanf(fp, "%s %d\n", key, &config->SO_TAXI);
+    fscanf(fp, "%s %d\n", key, &config->SO_SOURCES);
+    fscanf(fp, "%s %d\n", key, &config->SO_HOLES);
+    fscanf(fp, "%s %d\n", key, &config->SO_TOP_CELLS);
+    fscanf(fp, "%s %d\n", key, &config->SO_CAP_MIN);
+    fscanf(fp, "%s %d\n", key, &config->SO_CAP_MAX);
+    fscanf(fp, "%s %d\n", key, &config->SO_TIMENSEC_MIN);
+    fscanf(fp, "%s %d\n", key, &config->SO_TIMENSEC_MAX);
+    fscanf(fp, "%s %d\n", key, &config->SO_TIMEOUT);
+    fscanf(fp, "%s %d\n", key, &config->SO_DURATION);
+
+    fclose(fp);
+
+    /* creazione array ARGS per passare info ai processi creati */
+    args = malloc(NARGS * sizeof(char *));
+    for(i = 0; i < NARGS - 1; i++) args[i] = malloc(sizeof(int));
+    args[NARGS - 1] = NULL;
+
+    /* semaforo sync creazione sorgenti */
+    sync_sources_sem = semget(IPC_PRIVATE, 1, IPC_CREAT | 0600);
+    /* TEST_ERROR; */
+    semctl(sync_sources_sem, 0, SETVAL, config->SO_SOURCES); 
+
+    /* semaforo sync creazione taxi */
+    sync_taxi_sem = semget(IPC_PRIVATE, 1, IPC_CREAT | 0600);
+    /* TEST_ERROR; */
+    semctl(sync_taxi_sem, 0, SETVAL, config->SO_TAXI); 
+
+    END = 0;
+
+    pause_sem = semget(IPC_PRIVATE, 1, IPC_CREAT | 0600);
+    semctl(pause_sem, 0, SETVAL, 1);
+
+    return 0;
+}
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+void unload () {
+    int i, j;
+    int ret;
+
+    /* 1. args */
+    for(i = 0; i < NARGS; i++) free(args[i]);
+    free(args);
+
+    /* 2. semafori & pipes */
+    for(i = 0; i < SO_HEIGHT; i++) {
+        for(j = 0; j < SO_WIDTH; j++) {
+            if(!map[i][j].is_hole) semctl(map[i][j].cap_semid, 0, IPC_RMID);
+            if(map[i][j].source_pid > 0) semctl(map[i][j].req_access_sem, 0, IPC_RMID);  
+        }
+    }
+
+    semctl(sync_sources_sem, 0, IPC_RMID);
+
+    /* 3. map */
+    for(i = 0; i < SO_HEIGHT; i++) {
+        if(shmdt(map[i])) TEST_ERROR;
+        if(shmctl(map_row_ids[i], IPC_RMID, NULL)) TEST_ERROR;
+    }
+    
+    if (shmdt(map_row_ids)) TEST_ERROR;
+    if(shmctl(map_id, IPC_RMID, NULL)) TEST_ERROR;
+
+    /* 4. sim status */
 
 
-    /* switch(child = fork()) {
+    /* 5. close pipes (forse non serve) */
+}
+
+/* 
+################################################################################################# 
+                                           INIT WORLD
+descrizione
+################################################################################################# 
+*/
+void init_world () {
+    int i, j;
+
+    map_id = shmget(IPC_PRIVATE, sizeof(int) * SO_HEIGHT, /* S_IRUSR | S_IWUSR */ IPC_CREAT | 0600);
+    map_row_ids = shmat(map_id, NULL, 0);
+
+    for(i = 0; i < SO_HEIGHT; i++) {
+        map_row_ids[i] = shmget(IPC_PRIVATE, sizeof(Cell) * SO_WIDTH, IPC_CREAT | 0600);
+        map[i] = shmat(map_row_ids[i], NULL, 0);
+    }
+    
+
+    for(i = 0; i < SO_HEIGHT; i++) {
+        for(j = 0; j < SO_WIDTH; j++) {
+            map[i][j].is_hole = 0;
+            map[i][j].source_pid = 0;
+            map[i][j].req_access_sem = semget(IPC_PRIVATE, 1, IPC_CREAT | 0600); 
+            semctl(map[i][j].req_access_sem, 0, SETVAL, 0);
+            map[i][j].cap_semid = 0; 
+            map[i][j].cell_cap = config->SO_CAP_MIN + rand() % (config->SO_CAP_MAX - config->SO_CAP_MIN +1);
+            map[i][j].travel_time = config->SO_TIMENSEC_MIN + rand() % (config->SO_TIMENSEC_MAX - config->SO_TIMENSEC_MIN + 1);
+            map[i][j].traffic = 0;
+        }
+    }
+
+    gen_holes();
+
+    for(i = 0; i < SO_HEIGHT; i++) {
+        for(j = 0; j < SO_WIDTH; j++) {
+            if(!map[i][j].is_hole) {
+                map[i][j].cap_semid = semget(IPC_PRIVATE, 1, IPC_CREAT | 0600); 
+                /* TEST_ERROR; */
+                semctl(map[i][j].cap_semid, 0, SETVAL, map[i][j].cell_cap); 
+            }
+        }
+    }
+}
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+void gen_holes() {
+    int i, j; 
+    int count; 
+
+    count = 0; 
+    while (count < config->SO_HOLES) {
+        
+        i = rand() % SO_HEIGHT;
+        j = rand() % SO_WIDTH;
+
+        if (check_hole(i, j)) {
+            map[i][j].is_hole = 1; 
+            count++;
+        }
+    }
+}
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+int check_hole(int r, int c){
+    int i, j;
+
+    for(i = -1; i < 3; i++) {
+        for(j = -1; j < 3; j++) {
+            if ((r + i >= 0 && r + i < SO_HEIGHT) && (c + j >= 0 && c + j < SO_WIDTH) && map[r + i][c + j].is_hole){
+                return 0;
+            }   
+        }
+    }
+
+    return 1;
+}
+
+/* 
+################################################################################################# 
+                                            PROCESSES
+descrizione
+################################################################################################# 
+*/
+
+void gen_timer () {
+    int exec_ret;
+    sprintf(args[0], "%d", config->SO_DURATION);
+    sprintf(args[1], "%d", pause_sem);
+
+    switch(timer_id = fork()) {
         case -1:
             TEST_ERROR;
             exit(EXIT_FAILURE);
             break;
 
         case 0:
-            args = malloc(2 * sizeof(char *));
-
-            args[0] = malloc(sizeof(int));
-            sprintf(args[0],"%d", map_id);
-
-            args[1] = NULL;
-
-            execvp("./reader", args);
-
+            exec_ret = execvp("./out/timer", args);
+            fprintf(stderr, "||| errore nella exec del timer: %d |||\n", exec_ret);
             break;
 
         default:
             break;
-    } */
+    }
 }
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+void gen_sources () {
+    int count, child_pid;
+    int i, j;
+    int exec_ret;
+    
+    count = 0;
+    while(count < config->SO_SOURCES) {
+
+        i = rand() % SO_HEIGHT;
+        j = rand() % SO_WIDTH;
+
+        if(map[i][j].is_hole == 0 && map[i][j].source_pid == 0) {
+
+            pipe(map[i][j].req_pipe);
+        
+            switch(child_pid = fork()) {
+                case -1:
+                    TEST_ERROR;
+                    exit(EXIT_FAILURE);
+                    break;
+
+                case 0:
+                    sprintf(args[0],"%d", map_id);
+                    sprintf(args[1],"%d", i);
+                    sprintf(args[2],"%d", j);
+                    sprintf(args[3],"%d", sync_sources_sem);
+                    sprintf(args[4],"%d", pause_sem);
+
+                    exec_ret = execvp("./out/source", args);
+                    fprintf(stderr, "||| errore nella exec delle sources: %d |||\n", exec_ret);
+                    break;
+
+                default:
+                    map[i][j].source_pid = child_pid;
+                    close(map[i][j].req_pipe[W]);
+
+                    if(count == 0) child_gpid = child_pid; /* set first source as group leader */
+                    setpgid(child_pid, child_gpid); /* set all sources in the same group */
+                    break;
+            }
+
+            count++;
+        }
+    }
+}
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+void gen_taxi () { 
+    int i, j, count, child_pid;
+
+    count = 0;
+    while(count < config->SO_TAXI) {
+
+        i = rand() % SO_HEIGHT;
+        j = rand() % SO_WIDTH;
+
+        if(semctl(map[i][j].cap_semid, 0, GETVAL) > 0 && !map[i][j].is_hole) {
+            child_pid = gen_one_taxi(i, j);
+            /* setpgid(child_pid, child_gpid); */
+            count++;
+        }
+
+    }
+}
+
+int gen_one_taxi (int i, int j) {
+    int child_pid, exec_ret;
+
+    P(map[i][j].cap_semid, 0);
+
+    switch(child_pid = fork()) {
+        
+        case -1:
+            TEST_ERROR;
+            exit(EXIT_FAILURE);
+
+        case 0:
+            setpgid(getpid(), child_gpid);
+
+            sprintf(args[0], "%d", map_id);
+            sprintf(args[1], "%d", i);
+            sprintf(args[2], "%d", j);
+            sprintf(args[3], "%d", config->SO_TIMEOUT);
+            sprintf(args[4], "%d", sync_taxi_sem);
+            sprintf(args[5], "%d", pause_sem);
+            sprintf(args[6], "%d", END);
+
+            exec_ret = execvp("./out/taxi", args);
+            fprintf(stderr, "||| errore nella exec dei taxi: %d |||\n", exec_ret);
+            break;
+
+        default:
+            return child_pid;
+    }
+}
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+void respawn () {
+    int i, j, child;
+
+    do {
+        i = rand() % SO_HEIGHT;
+        j = rand() % SO_WIDTH;
+    } while (map[i][j].is_hole || semctl(map[i][j].cap_semid, 0, GETVAL) == 0);
+
+    child = gen_one_taxi(i, j);
+}
+
+/* 
+################################################################################################# 
+                                          SIMULATION
+descrizione
+################################################################################################# 
+*/
+void simulate () {
+    int i, j;
+    int child;
+    int status;
+
+    sync_simulation(pause_sem, 0, -1);
+
+    while((child = wait(&status)) > 0 && semctl(pause_sem, 0, GETVAL) == 0) {
+        if(WEXITSTATUS(status) == TAXI_ABRT) respawn();
+        else fprintf(stderr, "||| %s %d unexpectedly exited with code %d|||\n", child == timer_id ? "timer" : "taxi", WEXITSTATUS(status));
+    }
+
+    if(killpg(child_gpid, SIGTERM) < 0) TEST_ERROR;
+
+    if(waitpid(timer_id, &status, 0) > 0) {
+        printf("*** timer %d done ***\n", timer_id);
+        printf("((((( EVENT HORIZON )))))\n");
+    }
+
+    sync_simulation(pause_sem, 0, -1);
+
+    while((child = wait(&status)) > 0) {
+        switch (WEXITSTATUS(status)) {
+            case EXIT_SUCCESS:
+                printf("*** child %d terminated successfully ***\n", child);
+                break;
+            default:
+                fprintf(stderr, "||| child %d unexpectedly exited |||\n", child);
+        }
+    }
+
+    if(errno == ECHILD) printf("*** all children terminated successfully ***\n"); 
+
+}
+
+/* 
+################################################################################################# 
+                                            SIGNALS
+descrizione
+################################################################################################# 
+*/
 
 void set_signals() {
     struct sigaction sa;
 
-    /* set signal mask */
     sigemptyset(&mask);
-    sigaddset(&mask, SIGALRM); /* every second alarm */
+    sigaddset(&mask, SIGALRM);
 
     sa.sa_flags = SA_RESTART;
     sa.sa_mask = mask;
-
-    /* set behaviour to print map */
     sa.sa_handler = print_map_handler;
 
-    /* put process in listening to SIGALRM */
     sigaction(SIGALRM,&sa,NULL);
-    /* TEST_ERROR; */
 
-    /* set behaviour to wrap up */
+    sigemptyset(&mask);
+    sigaddset(&mask, SIGALRM);
+    sigaddset(&mask, SIGINT);
+    sigaddset(&mask, SIGTERM);
+
+    sa.sa_flags = 0;
+    sa.sa_mask = mask;
     sa.sa_handler = wrap_up;
-
-    /* put process in listening to SIGQUIT & SIGINT*/
-    sigaction(SIGQUIT,&sa,NULL);
-    /* TEST_ERROR; */
+    sigaction(SIGTERM,&sa,NULL);
     sigaction(SIGINT, &sa, NULL);
-    /* TEST_ERROR; */
 }
 
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
 void print_map_handler(int sig) {
-    killpg(source_gpid, SIGSTOP);
-    killpg(taxi_gpid, SIGSTOP);
+    killpg(child_gpid, SIGSTOP);
+    /* V(pause_sem, 0); */
 
-    print_map();
-
-    killpg(source_gpid, SIGCONT);
-    killpg(taxi_gpid, SIGCONT);
+        print_map();
+        
+    /* P(pause_sem, 0); */
+    killpg(child_gpid, SIGCONT);
 }
 
 void wrap_up(int sig) {
-    /* sigprocmask(SIG_BLOCK, &signal_mask, NULL); */
-
-    killpg(source_gpid, SIGQUIT);
-    killpg(taxi_gpid, SIGQUIT);
-
-    /* sigprocmask(SIG_UNBLOCK, &signal_mask, NULL); */
+    printf("--- TIME'S UP ---\n");
+    V(pause_sem, 0);
+    
 }
 
+/* 
+################################################################################################# 
+                                          MAP PRINT
+descrizione
+################################################################################################# 
+*/
+void print_map () {
+    int i, j;
+    char s[4];
+    int traffic;
 
+    printf("\n+");
 
+    for (j = 0; j < SO_WIDTH; j++) printf("---+");
 
+    for(i = 0; i < SO_HEIGHT; i++){
+        printf("\n|");
+        for(j = 0; j < SO_WIDTH; j++){
 
+            switch(map[i][j].is_hole) {
+                case 1: printf(HOLE "   " ENDSTYLE); break;
 
+                default:
+                    if (map[i][j].source_pid > 0) printf(SOURCE);
+                    if(!semctl(map[i][j].cap_semid, 0, GETVAL)) printf(BUSY);
+                    traffic = map[i][j].cell_cap - semctl(map[i][j].cap_semid, 0, GETVAL); 
+                    sprintf(s,"%2d ", traffic);
+                    printf("%s" ENDSTYLE, traffic == 0 ? "   " : s);
+            }
 
-
+            printf("|");
+        }
+    
+        printf("\n+");
+        for (j = 0; j < SO_WIDTH; j++){ printf("---+"); }
+    }
+    printf("\n\n");
+}
