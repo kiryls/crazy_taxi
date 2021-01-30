fbfe30ddd50f5ddafb817c419c53c8b01d422a18
kiryls <kiryl.shvachka@edu.unito.it>
2021-01-30 19:31:38 +0100

    lil change

--- headers/source.h
+++ headers/source.h
@@ -1,11 +1,12 @@
 
-Pos p;
-int tot_reqs;
-FILE * logp;
+Pos         p;
+int         tot_reqs;
+FILE        *logp;
+sigset_t    sleep_mask;
 
 void init(const char * argv[]);
 void write_log(FILE * fp, Pos dest);
 void set_signals();
-void termination(int sig);
 void gen_req(int sig);
+void termination(int sig);
 void resume(int sig);
