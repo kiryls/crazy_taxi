419a37fa943679ab4dc8636110c4108ede22ff9e
kiryls <kiryl.shvachka@edu.unito.it>
2021-01-30 19:31:12 +0100

    aggiunti i metodi inerenti il master

--- headers/master.h
+++ headers/master.h
@@ -1,12 +1,24 @@
 #ifndef MASTER
 #define MASTER
 
+Config*     config;
+char**      args;
+pid_t       child_gpid;
+pid_t       timer_id;
 
-
-void set_signals();
-void wrap_up(int sig);
-void signal_handler(int sig);
-void print_map_handler(int sig);
-void wrap_up(int sig);
+int     load ();
+void    unload ();
+void    init_world();
+void    gen_holes();
+int     check_hole(int x, int y);
+void    gen_timer();
+void    gen_sources ();
+void    gen_taxi ();
+int     gen_one_taxi (int i, int j);
+void    simulate ();
+void    set_signals();
+void    print_map_handler(int sig);
+void    wrap_up(int sig);
+void    print_map();
 
 #endif
