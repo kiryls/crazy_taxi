fd0048acd882d22f6c3781469a0948523cb62616
kiryls <kiryl.shvachka@edu.unito.it>
2021-02-01 11:22:57 +0100

    +report(), *get_direction(), *travel(), termination masks SIGTERM

--- src/taxi.c
+++ src/taxi.c
@@ -19,10 +19,6 @@ int main(int argc, char const *argv[]) {
     Z(sync_all);
 
     while(1) travel();
- 
-    /* get_req();
-
-    raise(SIGALRM); */
 
     exit(EXIT_FAILURE);
 }
@@ -41,12 +37,12 @@ void init (const char * argv[]) {
     p.c = atoi(argv[2]);
     TIMEOUT = atoi(argv[3]);
     sync_all = atoi(argv[4]);
+    ledger_id = atoi(argv[5]);
 
+    rep.taxi_id = getpid();
     rep.tot_length = 0;
-    rep.tot_time = 0;
+    rep.time = 0;
     rep.completed_rides = 0;
-
-    /* dest.r = dest.c = -1; */
 
     map_row_ids = (int*) shmat(map_id, NULL, 0);
 
@@ -54,6 +50,8 @@ void init (const char * argv[]) {
         map[i] = shmat(map_row_ids[i], NULL, 0);
 
     logp = fopen("/home/kiryls/Documents/Coding/project/logs/taxi.log", "a");
+
+    ledger = shmat(ledger_id, NULL, 0);
 
     write_log(logp);
 }
@@ -72,11 +70,47 @@ void write_log(FILE * logp) {
 }
 
 void report() {
-    if(on_duty) fprintf(logp, "%d on duty (%d,%d) --> (%d,%d): Tot cells=%d | Tot time=%d | Tot rides=%d\n", 
-                            getpid(), p.r+1, p.c+1, dest.r+1, dest.c+1, rep.tot_length, rep.tot_time, rep.completed_rides);
+    if(on_duty) fprintf(logp, "%d on duty (%d,%d) --> (%d,%d): Tot cells=%d | Tot time=%.3f | Tot rides=%d\n", 
+                            rep.taxi_id, p.r+1, p.c+1, dest.r+1, dest.c+1, rep.tot_length, 
+                            rep.time, rep.completed_rides);
 
-    else fprintf(logp, "%d on (%d,%d): Tot cells=%d | Tot time=%d | Tot rides=%d\n", 
-                            getpid(), p.r+1, p.c+1, rep.tot_length, rep.tot_time, rep.completed_rides);
+    else fprintf(logp, "%d on (%d,%d): Tot cells=%d | Tot time=%.3f | Tot rides=%d\n", 
+                            rep.taxi_id, p.r+1, p.c+1, rep.tot_length, 
+                            rep.time, rep.completed_rides);
+
+    if(rep.completed_rides == 0 && on_duty == 0) return; 
+
+    P(ledger->taxi_section);
+
+        ledger->completed_rides += rep.completed_rides;
+        ledger->failed_rides += on_duty;
+        
+        if(rep.tot_length > ledger->best_distance.tot_length) {
+
+            ledger->best_distance.taxi_id           = rep.taxi_id;
+            ledger->best_distance.tot_length        = rep.tot_length;
+            ledger->best_distance.time              = rep.time;
+            ledger->best_distance.completed_rides   = rep.completed_rides;
+        }
+
+        if(rep.time > ledger->best_longevity.time) {
+
+            ledger->best_longevity.taxi_id          = rep.taxi_id;
+            ledger->best_longevity.tot_length       = rep.tot_length;
+            ledger->best_longevity.time             = rep.time;
+            ledger->best_longevity.completed_rides  = rep.completed_rides;
+
+        } 
+
+        if(rep.completed_rides > ledger->most_rides.completed_rides) {
+
+            ledger->most_rides.taxi_id          = rep.taxi_id;
+            ledger->most_rides.tot_length       = rep.tot_length;
+            ledger->most_rides.time             = rep.time;
+            ledger->most_rides.completed_rides  = rep.completed_rides;
+        }
+
+    V(ledger->taxi_section);
 }
 
 void pretend_doing (int sec) {
@@ -107,30 +141,50 @@ Dir get_direction(){
     int dR, dC;
     Dir direction;
 
-    if ((dest.r - p.r) == 0 && (dest.c - p.c) == 0) return NO;
+    dR = (dest.r - p.r);
+    dC = (dest.c - p.c);
 
-    dR = (dest.r - p.r) > 0;
-    dC = (dest.c - p.c) > 0;
+
+    if (dR == 0 && dC == 0) return NO;
+
 
     /* e' piu' urgente andare in verticale o in orizzontale ? */
-    if(ABS(dest.c-p.c) > ABS(dest.r-p.r)) direction = dC * RIGHT + !dC * LEFT;
-    else direction = dR * DOWN + !dR * UP;
+    if(ABS(dC) > ABS(dR)) direction = (dC>0) * RIGHT + (dC<=0) * LEFT;
+    else direction = (dR>0) * DOWN + (dR<=0) * UP;
 
     switch (direction) {
         case UP:
-            if(map[p.r - 1][p.c].is_hole) return dC * RIGHT + !dC * LEFT;
+            if(map[p.r - 1][p.c].is_hole) direction = (dC>0) * RIGHT + (dC<=0) * LEFT;
             break;
 
         case DOWN:
-            if(map[p.r + 1][p.c].is_hole) return dC * RIGHT + !dC * LEFT;
+            if(map[p.r + 1][p.c].is_hole) direction = (dC>0) * RIGHT + (dC<=0) * LEFT;
             break;
 
         case LEFT:
-            if(map[p.r][p.c - 1].is_hole) return dR * DOWN + !dR * UP;
+            if(map[p.r][p.c - 1].is_hole) direction = (dR>0) * DOWN + (dR<=0) * UP;
             break;
         
         case RIGHT:
-            if(map[p.r][p.c + 1].is_hole) return dR * DOWN + !dR * UP;
+            if(map[p.r][p.c + 1].is_hole) direction = (dR>0) * DOWN + (dR<=0) * UP;
+            break;
+    }
+
+    switch (direction) {
+        case UP:
+            if(p.r-1 < 0) return DOWN;
+            break;
+
+        case DOWN:
+            if(p.r+1 > SO_HEIGHT-1) return UP;
+            break;
+
+        case LEFT:
+            if(p.c-1 < 0) return RIGHT;
+            break;
+        
+        case RIGHT:
+            if(p.c + 1 > SO_WIDTH-1) return LEFT;
             break;
     }
 
@@ -147,21 +201,25 @@ descrizione
 
 void travel () {
     Dir dir;
+    float t;
 
     alarm(TIMEOUT); 
 
     on_duty = 0;
-    /* dest.r = dest.c = -1; */ /* set no goal */
+
     if(map[p.r][p.c].source_pid > 0) { get_req(); alarm(0); on_duty = 1; } 
     else { alarm(0); sleep(1); raise(SIGALRM); }
+
+    t = 0;
 
     while((dir = get_direction()) != NO) {
         move(dir);
 
         rep.tot_length++;
-        rep.tot_time += map[p.r][p.c].travel_time;
+        t += (float) map[p.r][p.c].travel_time / 1000000000;
     }
 
+    if(t > rep.time) rep.time = t;
     rep.completed_rides++;
 }
 
@@ -177,15 +235,13 @@ void move (Dir dir) {
     }
 
     t.tv_nsec = map[next.r][next.c].travel_time;
-    t.tv_sec = 0;
-
+    t.tv_sec  = 0;
     nanosleep(&t, (struct timespec *) NULL);
     
     alarm(TIMEOUT);
 
     P(map[next.r][next.c].cap_semid);
     alarm(0);
-    
     V(map[p.r][p.c].cap_semid);
 
     p.r = next.r;
@@ -208,6 +264,8 @@ void set_signals () {
     sigemptyset(&mask);
     sigaddset(&mask, SIGALRM);
     sigaddset(&mask, SIGSTOP);
+    sigaddset(&mask, SIGTERM);
+    /* sigaddset(&mask, SIGINT); */
     sa.sa_flags = 0;
     sa.sa_mask = mask;
     sa.sa_handler = termination;
@@ -236,6 +294,7 @@ void termination (int sig) {
     if(shmdt(map_row_ids)) TEST_ERROR;
 
     report();
+    if(shmdt(ledger)) TEST_ERROR;
 
     fprintf(logp, "unloaded %d\n\n", getpid());
     fclose(logp);
